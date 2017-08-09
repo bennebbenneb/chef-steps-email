7a1a932fcfe76b290afa29b5f03daada3cff393b
Ben <bensisson1@gmail.com>
2017-08-09 13:27:25 -0700

    added a test. created a build

--- ui/__tests__/emailDedupe.test.js
+++ ui/__tests__/emailDedupe.test.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import emailDedupe from "../src/emailDedupe/emailDedupe";
 import emailsTest1 from "../public/stubs/emails.test1.json";
+import emailsTest2 from "../public/stubs/emails.test2.json";
 import largeEmailSet from "../public/stubs/emails.json";
 
 describe("A Dedupe email test", () => {
@@ -8,7 +8,19 @@ describe("A Dedupe email test", () => {
         const dedupedEmails = emailDedupe(emailsTest1);
         expect(dedupedEmails).toEqual([
             "598a29bba95d32c840be705e@fake.com"
-        ])
+        ]);
+    });
+
+    it("keeps the same order", () => {
+        const dedupedEmails = emailDedupe(emailsTest2);
+        expect(dedupedEmails).toEqual([
+            "1@fake.com",
+            "2@fake.com",
+            "3@fake.com",
+            "4@fake.com",
+            "5@fake.com",
+            "6@fake.com"
+        ]);
     });
 
     it("completes in less than a second", () => {
@@ -17,6 +29,6 @@ describe("A Dedupe email test", () => {
         const endTime = new Date().getTime();
         const totalTime = endTime - startTime;
         const oneSecond = 1000;
-        expect(totalTime).toBeLessThan(oneSecond)
+        expect(totalTime).toBeLessThan(oneSecond);
     });
 });
